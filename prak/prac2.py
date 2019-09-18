711bc71bb4535a61ec6d86813d52a53d0c22018b
Anastasia <nastya.lem12@gmail.com>
2019-09-18 15:17:42 +0300

    включен import, убран тип integer

--- prak/prac2.py
+++ prak/prac2.py
@@ -1,72 +1,74 @@
 
-class Node: 
-	def __init__(self, value = None, next = None):
-		self.value = value
-		self.next = next
+# class Node: 
+# 	def __init__(self, value = None, next = None):
+# 		self.value = value
+# 		self.next = next
 
-class MyList(object):
-	def __init__(self):
-		self.length = 0
-		self.first = None
-		self.last = None
+# class MyList(object):
+# 	def __init__(self):
+# 		self.length = 0
+# 		self.first = None
+# 		self.last = None
 
-	def __str__(self):
-		if self.first != None:
-			current = self.first
-			out = 'LinkedList [' + str(current.value) 
-			while current.next != None:
-				current = current.next
-				out += ',' + str(current.value)
-			return out + ']'
-		return 'LinkedList []'
+# 	def __str__(self):
+# 		if self.first != None:
+# 			current = self.first
+# 			out = 'LinkedList [' + str(current.value) 
+# 			while current.next != None:
+# 				current = current.next
+# 				out += ',' + str(current.value)
+# 			return out + ']'
+# 		return 'LinkedList []'
 
-	def clear(self):
-		self.__init__()
+# 	def clear(self):
+# 		self.__init__()
 		
-	def add(self, x):
-		self.length += 1
-		if self.first == None:
-			self.last = self.first = Node(x, None)
-		else:
-			self.last.next = self.last = Node(x, None)
+# 	def add(self, x):
+# 		self.length += 1
+# 		if self.first == None:
+# 			self.last = self.first = Node(x, None)
+# 		else:
+# 			self.last.next = self.last = Node(x, None)
 
-	def find (self, key):
-		current = None
-		if self.first != None:
-			current = self.first
-			while key != current.value or current.next != None:
-				current = current.next
-			if key != current.value: 
-				current = None
-		return current
+# 	def find (self, key):
+# 		current = None
+# 		if self.first != None:
+# 			current = self.first
+# 			while key != current.value or current.next != None:
+# 				current = current.next
+# 			if key != current.value: 
+# 				current = None
+# 		return current
 
-	def deletekey(self, key):
-		self.length -=1
-		if self.first != None:
-			current = self.first
-			while key != current.value or current.next != None:
-				previous = current
-				current = current.next
-			if key == current.value:
-				previous.next = current.next
-			return
+# 	def deletekey(self, key):
+# 		self.length -=1
+# 		if self.first != None:
+# 			current = self.first
+# 			while key != current.value or current.next != None:
+# 				previous = current
+# 				current = current.next
+# 			if key == current.value:
+# 				previous.next = current.next
+# 			return
 
-	def deletelast(self):
-		self.length -= 1
-		if self.first != None:
-			self.last = None
-		return
+# 	def deletelast(self):
+# 		self.length -= 1
+# 		if self.first != None:
+# 			self.last = None
+# 		return
 
-def Transformation(num):
-	lst = MyList()
-	if num == 0:
-		lst.add(0)
-		return lst
-	while num != 0:
-		key = num % 10
-		num //= 10
-		lst.add(key)
-	return lst
+# def Transformation(num):
+# 	lst = MyList()
+# 	if num == 0:
+# 		lst.add(0)
+# 		return lst
+# 	while num != 0:
+# 		key = num % 10
+# 		num //= 10
+# 		lst.add(key)
+# 	return lst
+
+from prac1 import *
 
 def Sum(lst1, lst2):
 	lenmax = max(lst1.length, lst2.length)
@@ -102,7 +104,7 @@ def Sum(lst1, lst2):
 	return lstsum
 
 if __name__ == '__main__':
-	number1 = int(input())
+	number1 = input()
 	lst1 = Transformation(number1)
 	number2 = int(input())
 	lst2 = Transformation(number2)
